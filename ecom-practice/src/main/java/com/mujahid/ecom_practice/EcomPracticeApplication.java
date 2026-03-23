9b61eeecf62662a2d291870b74a65f9cd6060fe7
abdalrhman <abdalrhmanmogahed2002@gmail.com>
2026-03-23 14:21:47 +0200

    initial commit for ecom project

---
+++ ecom-practice/src/main/java/com/mujahid/ecom_practice/EcomPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.mujahid.ecom_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomPracticeApplication.class, args);
+	}
+
+}
---
+++ ecom-practice/src/test/java/com/mujahid/ecom_practice/EcomPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mujahid.ecom_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
