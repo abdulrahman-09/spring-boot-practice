21aa2d945a27fc076fbcdfbb753106b6a7257acc
abdalrhman <abdalrhmanmogahed2002@gmail.com>
2026-05-04 22:38:06 +0300

    Try pasword compromised checker, debug the DaoAuthenctication provider internal flow

--- spring-security-lab/src/main/java/com/am9/spring_security_lab/config/SecurityConfig.java
+++ spring-security-lab/src/main/java/com/am9/spring_security_lab/config/SecurityConfig.java
@@ -2,9 +2,18 @@ package com.am9.spring_security_lab.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -18,5 +27,30 @@ public class SecurityConfig {
         http.httpBasic(withDefaults());
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("am")
+                        .password("{bcrypt}$2a$12$b35ROobBwrlZd1rvhtw.yON8ojShxEBnCvTijBgemX6Y5sIhGW96y")
+                        .roles("admin")
+                        .build(),
+                User.withUsername("jow")
+                        .password("{bcrypt}$2a$12$0cT./hnCg2fCKqRDRzzAxu8hGe8AA3C7rQYcTYEAsDAndH0Hdimkq")
+                        .roles("user")
+                        .build()
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+//    @Bean
+//    public CompromisedPasswordChecker compromisedPasswordChecker(){
+//        return new HaveIBeenPwnedRestApiPasswordChecker();
+//    }
+
 
 }
