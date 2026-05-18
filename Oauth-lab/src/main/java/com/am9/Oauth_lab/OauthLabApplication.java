00317c98f873329a0e759f19ec568ffd1fcd0a62
abdalrhman <abdalrhmanmogahed2002@gmail.com>
2026-05-18 15:42:48 +0300

    Initial commit for Oauth demo app

---
+++ Oauth-lab/src/main/java/com/am9/Oauth_lab/OauthLabApplication.java
@@ -0,0 +1,13 @@
+package com.am9.Oauth_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthLabApplication.class, args);
+	}
+
+}
---
+++ Oauth-lab/src/test/java/com/am9/Oauth_lab/OauthLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.am9.Oauth_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
